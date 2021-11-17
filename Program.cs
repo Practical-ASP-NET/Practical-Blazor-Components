043ec92da1f6b0c6c3fa5ab345e96f81dd46a28c
Jon Hilton <hilton.jon@gmail.com>
2021-11-17 13:55:09 +0000

    Kanban layout

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using KanbanBoard;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
